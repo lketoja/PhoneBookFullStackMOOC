04a3166341c1495fbaa8b4f2b69cf0ed626a821e
Lotta Ketoja <lotta.ketoja@gmail.com>
2020-03-19 08:45:32 +0200

    add ui build

---
+++ build/precache-manifest.4a07b4ef03745b8b2f73e45f9b70ffa3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63766ebe4c96ee8f9a383851b2734c09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf7f65def6f797af9269",
+    "url": "/static/js/2.846221ea.chunk.js"
+  },
+  {
+    "revision": "8ff4e7ab0e5896c96048",
+    "url": "/static/js/main.1f84bab1.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
